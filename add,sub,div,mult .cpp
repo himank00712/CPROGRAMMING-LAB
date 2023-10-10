9c5122856514e40425442153d847a99db9dee899
himank00712 <146538792+himank00712@users.noreply.github.com>
2023-10-10 14:04:21 +0530

    addition ,subtraction ,multplication and division.

---
+++ add,sub,div,mult .cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+
+{
+	float a, b;
+	printf("enter any two values");
+	scanf_s("%f%f", &a, &b);
+	printf("addition is %f", a + b);
+	printf("subtraction is %f", a - b);
+	printf("multiplication is %f", a * b);
+	printf("division is %f", a / b);
+
+}
